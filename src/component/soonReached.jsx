e38ef24634ad1c3f125e6ff02a9443e649aa99ac
Rahul <rahul.dewal@ahom.tech>
2024-01-20 19:49:26 +0530

    chanbge id for avoid warning and lit css

--- src/component/soonReached.jsx
+++ src/component/soonReached.jsx
@@ -4,7 +4,7 @@ const soonReached = (ProductCard) => {
   return (props)=>{
     return(
         <>
-            <p className='absolute p-1 left-6 top-3  bg-black rounded text-white z-20'>Soon</p>
+            <p className='absolute p-1 left-6 top-3  bg-red-700 rounded text-white z-20'>Soon</p>
             <ProductCard {...props}/>
         </>
     )
